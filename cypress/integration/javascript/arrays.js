3e4705f0aedebdb9322615b99fba59d44a45b1f0
unknown <satvik.desamala@gmail.com>
2021-04-11 11:11:27 +0530

    array.js added

---
+++ cypress/integration/javascript/arrays.js
@@ -0,0 +1,27 @@
+var arr = [1, 2, 3, 4, 5]
+// console.log(arr.length)
+// console.log(arr)
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log('arr[' + i + ']' + ' : ' + arr[i])
+// }
+
+// for (var i = 0; i <= arr.length - 1; i++) {
+//     console.log('arr[' + i + ']' + ' : ' + arr[i])
+// }
+
+// console.log(arr[3])
+
+
+// var number1 = arr[0]
+// console.log('total: ' + (number1 + arr[1])) //BODMAS
+
+var number1 = arr[0]
+var number2 = arr[1]
+var total = number1 + number2
+// console.log('total: ' + total)
+
+console.log('total: ' + (arr[0] + arr[1]))
+console.log('2*3: ' + (arr[1] * arr[2]))
+console.log('4/2: ' + (arr[3] / arr[1]))
+
